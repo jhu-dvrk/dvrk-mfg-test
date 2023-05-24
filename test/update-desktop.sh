41da07c13cf8a4c67721c6b5b5659f938665e8b2
Peter Kazanzides <pkaz@jhu.edu>
2023-05-24 09:25:24 -0400

    Reverted desktop files

--- test/update-desktop.sh
+++ test/update-desktop.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo 'Setting up desktop'
 # $HOME and ~ do not work in desktop files, so use sed to replace
-# ~ with the user's home directory when copying to ~/Desktop
-sed "s|~|${HOME}|g" scripts/Desktop/conntest.desktop  > ~/Desktop/conntest.desktop
-sed "s|~|${HOME}|g" scripts/Desktop/dqlatest1.desktop > ~/Desktop/dqlatest1.desktop
-sed "s|~|${HOME}|g" scripts/Desktop/dqlatest2.desktop > ~/Desktop/dqlatest2.desktop
-sed "s|~|${HOME}|g" scripts/Desktop/dractest.desktop  > ~/Desktop/dractest.desktop
-sed "s|~|${HOME}|g" scripts/Desktop/swupdate.desktop  > ~/Desktop/swupdate.desktop
+# /home/jhu with the user's home directory when copying to ~/Desktop
+sed "s|/home/jhu|${HOME}|g" scripts/Desktop/conntest.desktop  > ~/Desktop/conntest.desktop
+sed "s|/home/jhu|${HOME}|g" scripts/Desktop/dqlatest1.desktop > ~/Desktop/dqlatest1.desktop
+sed "s|/home/jhu|${HOME}|g" scripts/Desktop/dqlatest2.desktop > ~/Desktop/dqlatest2.desktop
+sed "s|/home/jhu|${HOME}|g" scripts/Desktop/dractest.desktop  > ~/Desktop/dractest.desktop
+sed "s|/home/jhu|${HOME}|g" scripts/Desktop/swupdate.desktop  > ~/Desktop/swupdate.desktop
 gio set ~/Desktop/conntest.desktop  metadata::trusted true
 gio set ~/Desktop/dqlatest1.desktop metadata::trusted true
 gio set ~/Desktop/dqlatest2.desktop metadata::trusted true
